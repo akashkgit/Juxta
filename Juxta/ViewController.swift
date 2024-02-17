a0aff62a7cec6947f2fbe2cb0b9529ca3a9a230c
akashkumar <akashkvit@gmail.com>
2024-02-17 17:02:52 -0500

    Initial Commit

---
+++ Juxta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Juxta
+//
+//  Created by akash kumar on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
